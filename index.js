46d3b654c54a25d0cf9c9fb47e8635cf2f388966
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-05-24 21:02:18 +0300

    Update

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 const countriesByContinent = {};
 
-
-const loadCountryAPI = () =>{
-    
+const loadCountryAPI = () => {
     fetch('data.json')
-    .then(res => res.json())
-    .then(data => displayCountries(data))
-    .then(data => {
-        data.countries.forEach(country => {
-            if (!countriesByContinent[country.region]) {
-                countriesByContinent[country.region] = [];
-            }
-            countriesByContinent[country.region].push(country);
-        });
-    })
-}
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(country => {
+                if (!countriesByContinent[country.region]) {
+                    countriesByContinent[country.region] = [];
+                }
+                countriesByContinent[country.region].push(country);
+            });
+            displayCountries(data); 
+        })
+        .catch(error => console.error('Error fetching the JSON data:', error));
+};
 
-const displayCountries = countries =>{
-    const countriesHTML = countries.map(country => getCountry(country));
+const displayCountries = countries => {
+    const countriesHTML = countries.map(country => getCountryHTML(country));
     const container = document.getElementById('countries');
-    container.innerHTML = countriesHTML.join(' ');
-}
+    container.innerHTML = countriesHTML.join('');
+};
 
-const getCountry = (country) =>{
+const getCountryHTML = country => {
     return `
         <div class="country-div">
-        <img src="${country.flags.png}">
-        <h2>${country.name}</h2>
-        <hr>
-        <h4>Population: ${country.population}</h4>
-        <h4>Regoin: ${country.region}</h4>
-        <h4>Capital: ${country.capital}</h4>
+            <img src="${country.flags.png}" alt="Flag of ${country.name}">
+            <h2>${country.name}</h2>
+            <hr>
+            <h4>Population: ${country.population}</h4>
+            <h4>Region: ${country.region}</h4>
+            <h4>Capital: ${country.capital}</h4>
         </div>
-    `
-}
+    `;
+};
 
-loadCountryAPI()
-
-// sorting per continent
+loadCountryAPI();
 
 document.addEventListener('DOMContentLoaded', () => {
     const continentSelect = document.getElementById('continent');
     const countryList = document.getElementById('countries');
     const themeToggle = document.getElementById('theme-toggle');
+    const searchInput = document.getElementById('search');
 
     continentSelect.addEventListener('change', () => {
         const selectedContinent = continentSelect.value;
         updateCountryList(selectedContinent);
+    });
+
+    searchInput.addEventListener('input', () => {
+        const query = searchInput.value.toLowerCase();
+        if (query) {
+            const filteredCountries = searchCountry(query);
+            displayCountries(filteredCountries);
+        } else {
+            updateCountryList(continentSelect.value);
+        }
     });
 
     themeToggle.addEventListener('click', () => {
@@ -59,7 +67,12 @@ document.addEventListener('DOMContentLoaded', () => {
             const countries = countriesByContinent[continent];
             displayCountries(countries);
         } else {
-            countryList.innerHTML = '<p>No countries available for the selected continent.</p>';
+            displayCountries([]);
         }
+    }
+
+    function searchCountry(query) {
+        const allCountries = Object.values(countriesByContinent).flat();
+        return allCountries.filter(country => country.name.toLowerCase().includes(query));
     }
 });
